4c001d68db8c62e480e465f70feb0e388828d1e9
Pavel <pavel.muratev@gmail.com>
2019-09-23 23:14:27 +0300

    Fix filling of path matrix

--- main.cpp
+++ main.cpp
@@ -18,7 +18,8 @@ int main(int argc, char *argv[])
         qPath[i].fill(10000,N);
     }
 
-    for(int i = 0; i<N; i++) {
+
+    while(!in.atEnd()){
         QStringList line = in.readLine().split(" ");
         qPath[line[0].toInt()][line[1].toInt()] =  qPath[line[1].toInt()][line[0].toInt()] = line[2].toInt();
     }
